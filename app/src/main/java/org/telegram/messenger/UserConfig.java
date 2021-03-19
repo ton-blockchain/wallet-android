f21d86fd42c72531aed384bc36f5bc770b39c493
Vladislav Zhuk <vladislav.zhuk2303@gmail.com>
2021-03-19 14:10:37 +0300

    Prepare for publish
    Fix make for windows
    Change appId, sdk version, name wallet, blockchain url, blockchain config

--- app/src/main/java/org/telegram/messenger/UserConfig.java
+++ app/src/main/java/org/telegram/messenger/UserConfig.java
@@ -129,7 +129,7 @@ public class UserConfig extends BaseController {
                 }
             }
             walletConfig[NETWORK_TYPE_TEST] = preferences.getString("walletConfig", "");
-            walletConfigUrl[NETWORK_TYPE_TEST] = preferences.getString("walletConfigUrl", "https://ton.org/config-test.json");
+            walletConfigUrl[NETWORK_TYPE_TEST] = preferences.getString("walletConfigUrl", "https://newton-blockchain.github.io/global.config.json");
             walletConfigType[NETWORK_TYPE_TEST] = preferences.getInt("walletConfigType", TonController.CONFIG_TYPE_URL);
             walletBlockchainName[NETWORK_TYPE_TEST] = preferences.getString("walletBlockchainName", "testnet2");
             walletConfigFromUrl[NETWORK_TYPE_TEST] = preferences.getString("walletConfigFromUrl", "");
